eeb67a0e4428cf9e3a9874232a84b661e3274b5a
carlosdantasjunior <110200367+carlosdantasjunior@users.noreply.github.com>
2025-01-19 22:37:52 -0300

    Add files via upload
    sistema de senha em JS

---
+++ js/app.js
@@ -0,0 +1,12 @@
+alert ('Digite a senha do sistema' );
+let senhaDoSistema = "senhaTeste";
+let senha 
+while (senha != senhaDoSistema) {
+    senha = prompt("Digite a senha do sistema:");
+
+if (senha == senhaDoSistema){
+    alert("Acesso ao sistema garantido");
+}else{
+    alert("senha incorreta");
+}
+}
